d5003593cac145c72850b76d783f42f5af9004d9
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-07-21 22:00:47 +0200

    added validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 const userSchema = new Schema({
     email: {
         type: String,
@@ -15,14 +16,25 @@ const userSchema = new Schema({
 })
 //static signup method
 
-userSchema.statics.signup = async function(email, password){
-    const emailExists =await this.findOne({email})
-    if (emailExists){
+userSchema.statics.signup = async function (email, password) {
+    //validation
+    if (!email || !password) {
+        throw Error('All fields are required')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Invalid email')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
+    const emailExists = await this.findOne({ email })
+    if (emailExists) {
         throw Error('Email already in use')
     }
-    const salt= await bcrypt.genSalt(10)
+    const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
-    const user = await this.create({email, password: hashedPassword})
+    const user = await this.create({ email, password: hashedPassword })
     return user;
 }
 
