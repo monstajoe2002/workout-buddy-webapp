dfbb4992245d5dc2211d1352367a2bc93e09932e
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-07-27 13:01:49 +0200

    login user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -37,5 +37,21 @@ userSchema.statics.signup = async function (email, password) {
     const user = await this.create({ email, password: hashedPassword })
     return user;
 }
+//static login method
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error('All fields are required')
+    }
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error('Invalid email')
+    }
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+        throw Error('Invalid password')
+    }
+    return user;
+}
+
 
 module.exports = mongoose.model('User', userSchema)
