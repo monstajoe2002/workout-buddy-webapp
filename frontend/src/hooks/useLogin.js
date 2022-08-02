97e6cc4e06d1cc20e18d282b4f575e4815e158b4
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-08-02 15:02:30 +0200

    login hook

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export function useLogin() {
+    const [error, setError] = useState(null);
+    const [isLoading, setisLoading] = useState(null);
+    const { dispatch } = useAuthContext();
+    const login = async (email, password) => {
+        setisLoading(true);
+        setError(null);
+
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const json = await response.json();
+        if (!response.ok) {
+            setisLoading(false);
+            setError(json.error);
+        }
+        if (response.ok) {
+            //save the user to local storage
+            localStorage.setItem('user', JSON.stringify(json));
+            //update AuthContext
+            dispatch({ type: 'LOGIN', payload: json });
+            setisLoading(false);
+        }
+
+    }
+    return { login, error, isLoading };
+};
+
+
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 export default function Login() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const handleSubmit = (e) => {
+    const { login, error, isLoading } = useLogin()
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(email, password)
+        
+        await login(email, password)
     }
     return (
         <form className="login" onSubmit={handleSubmit}>
@@ -17,7 +20,8 @@ export default function Login() {
             <input type="password"
                 onChange={e => setPassword(e.target.value)}
                 value={password} />
-            <button>Log in</button>
+            <button disabled={isLoading}>Log in</button>
+            {error && <div className="error">{error}</div>}
         </form>
     )
 }
